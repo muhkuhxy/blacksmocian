6ba939ae574f55eaf14bfba0d115e8fefb82c7bc
muhkuhxy <muhkuhxy@github.com>
2022-11-23 12:40:53 +0000

    formatting

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				ember: 'rgb(227, 69, 44)',
-				spark: 'rgb(255, 174, 0)',
-			}
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        ember: 'rgb(227, 69, 44)',
+        spark: 'rgb(255, 174, 0)',
+      }
+    },
+  },
+  plugins: [],
 }
